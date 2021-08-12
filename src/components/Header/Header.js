082a50c8b80a06b35a724fefe9f0f989facc175a
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-08-12 17:29:00 -0500

    adds header and container components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import { FaShoppingCart } from "react-icons/fa";
+import Container from "@components/Container";
+
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container className={styles.headerContainer}>
+        <p className={styles.headerTitle}>Andrew&apos;s Store</p>
+        <p className={styles.headerCart}>
+            
+        <FaShoppingCart />
+            $0.00</p>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
